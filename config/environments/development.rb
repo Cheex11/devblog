6b104fdad65698cf521eb1142daa75b3bb980087
Cody Wilson <Cheex11@gmail.com>
2014-08-03 19:21:31 -0500

    added devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Recommended settings from Devise (setup default URL options for the Devise mailer in each environment).
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
